89fbc0f373cc2d1afc1d27d493917733503adc08
Davide Vozzi <davide.vozzi@gmail.com>
2016-06-02 20:20:02 +0200

    UI/UX upgrade and video control

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,163 @@
+// ======================= DOM Utility Functions from PastryKit =============================== //
+
+// Sure, we could use jQuery or XUI for these,
+// but these are concise and will work with plain vanilla JS
+
+Element.prototype.hasClassName = function (a) {
+	return new RegExp("(?:^|\\s+)" + a + "(?:\\s+|$)").test(this.className);
+};
+
+Element.prototype.addClassName = function (a) {
+	if (!this.hasClassName(a)) {
+		this.className = [this.className, a].join(" ");
+	}
+};
+
+Element.prototype.removeClassName = function (b) {
+	if (this.hasClassName(b)) {
+		let a = this.className;
+		this.className = a.replace(new RegExp("(?:^|\\s+)" + b + "(?:\\s+|$)", "g"), " ");
+	}
+};
+
+Element.prototype.toggleClassName = function (a) {
+	this[this.hasClassName(a) ? "removeClassName" : "addClassName"](a);
+};
+
+// YOUTUBE API (disabled! before enabling this be sure to disable 'VIDEO PLAYER')//
+// let tag = document.createElement('script');
+// tag.src = 'https://www.youtube.com/player_api';
+// let firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// let tv, playerDefaults = {
+// 	autoplay: 0,
+// 	autohide: 1,
+// 	modestbranding: 0,
+// 	rel: 0,
+// 	showinfo: 0,
+// 	controls: 0,
+// 	disablekb: 1,
+// 	enablejsapi: 0,
+// 	iv_load_policy: 3
+// };
+// let vid = [
+// 	{
+// 	'videoId': 'YQKgBmb2WoU',
+// 	'startSeconds': 515,
+// 	'endSeconds': 690,
+// 	'suggestedQuality': 'hd720'
+// 	},
+// 	{
+// 	'videoId': '9ge5PzHSS0Y',
+// 	'startSeconds': 465,
+// 	'endSeconds': 657,
+// 	'suggestedQuality': 'hd720'
+// 	},
+// 	{
+// 	'videoId': 'OWsCt7B-KWs',
+// 	'startSeconds': 0,
+// 	'endSeconds': 240,
+// 	'suggestedQuality': 'hd720'
+// 	},
+// 	{
+// 	'videoId': 'qMR-mPlyduE',
+// 	'startSeconds': 19,
+// 	'endSeconds': 241,
+// 	'suggestedQuality': 'hd720'
+// 	}
+// ]
+//
+// function onYouTubePlayerAPIReady() {
+// 	tv = new YT.Player('tv', {
+// 		events: {
+// 			'onReady': onPlayerReady,
+// 			'onStateChange': onPlayerStateChange
+// 		},
+// 		playerVars: playerDefaults
+// 	});
+// }
+//
+// function onPlayerReady() {
+// 	tv.loadVideoById(vid[randomvid]);
+// 	tv.mute();
+// }
+// function onPlayerStateChange(e) {
+// 	if (e.data === 1) {
+// 		$('#tv').addClass('active');
+// 	} else if (e.data === 0) {
+// 		tv.seekTo(vid[randomvid].startSeconds);
+// 	}
+// }
+// function vidRescale() {
+// 	var w = $(window).width() + 200, h = $(window).height() + 200;
+// 	if (w / h > 16 / 9) {
+// 		tv.setSize(w, w / 16 * 9);
+// 		$('.tv .screen').css({ 'left': '0px' });
+// 	} else {
+// 		tv.setSize(h / 9 * 16, h);
+// 		$('.tv .screen').css({ 'left': -($('.tv .screen').outerWidth() - w) / 2 });
+// 	}
+// }
+//
+// $(window).on('load resize', function () {
+// 	vidRescale();
+// });
+//
+// $('.hi span').on('click', function () {
+// 	$('#tv').toggleClass('mute');
+// 	if ($('#tv').hasClass('mute')) {
+// 		tv.mute();
+// 	} else {
+// 		tv.unMute();
+// 	}
+// });
+
+// VIDEO PLAYER
 
 $(document).ready(function(){
-	let iOS = /iPad|iPhone|iPod/.test(navigator.platform);
-	if( iOS ) {
-	    var background_videos = document.querySelectorAll('.bgvid');
-	    for( i=0; i<background_videos.length; i++ ) {
-	        background_videos[i].parentNode.removeChild(background_videos[i]);
-	    }
-	}
 
-	let fade_in_videos = document.querySelectorAll('.fade-in-video');
-	for( i=0; i<fade_in_videos.length; i++ ) {
-	    fade_in_videos[i].addEventListener("playing", function(){
-	        this.className += ' is-playing';
-	    });
-	}
+	let duration, bypass = true;
+	let vid1 = document.getElementsByClassName('fade-in-video')[0];
+
+	vid1.addEventListener("durationchange", function(e){
+		duration = this.duration;
+	});
+	vid1.addEventListener("timeupdate", function(e){
+		if(vid1.currentTime > duration - 0.6 && bypass) {
+			bypass = false;
+			this.classList.remove('is-playing');
+			setTimeout(function(){
+				vid1.currentTime = 0;
+				bypass = true;
+			}, 600);
+		};
+
+	});
+
 })
 
 $(window).on('load', function(){
-	$('body').addClass('active');
+
+	let vid1 = document.getElementsByClassName('fade-in-video')[0];
+	vid1.load();
+	vid1.addEventListener("canplay", function(e){
+		if(vid1.currentTime === 0) {
+			vid1.play();
+		};
+	});
+
+	vid1.addEventListener("playing", function(e){
+		vid1.classList.add('is-playing');
+	});
+
+// FLIP PLAY PAUSE ICONS
+
+	let card = document.getElementsByClassName('card')[0];
+	card.addEventListener('click', function() {
+		switchState(vid1);
+		card.toggleClassName('flipped');
+	});
+
 	$('.container').show(function(){
 		$('.center-wrapper').fadeOut(0).delay(800).fadeIn(1200, 'swing', function(){
 			$('.tile').fadeTo(0, 0.5, 'linear')
@@ -27,14 +167,20 @@ $(window).on('load', function(){
 			.delay(150).fadeTo(0, 0.7, 'linear', function(){
 				$('#startBtn').css('background-color', 'hsl(0, 41%, 43%)');
 				$('#strictBtn').css('background-color', 'hsl(48, 46%, 41%)');
-				$('.center').css('background-color', '#999');
-				$('#cog_icon').fadeIn(900);
-				$('body').removeClass('active');
+				$('.center-wrapper').css('background-color', 'rgba(171, 171, 171, 0.8)');
+				$('#infoIc, .vidPPWrapper').fadeIn(600);
+				$('.hi').addClass('flipIn');
 			});
 		});
 	});
 });
 
+function switchState(v) {
+	if (v.paused == true) v.play();
+	else v.pause();
+}
+
+/* SIMON GAME */
 
 let sequence = [];
 let copy = [];
@@ -48,12 +194,11 @@ simon.masterSw = false;
 simon.strict = false;
 
 simon.start = function() {
-
 	simon.reset();
 	delay = setTimeout(function() {
 		advanceTurn();
 		playSeq();
-	},1500);
+	}, 1500);
 }
 
 simon.reset = function() {
@@ -66,23 +211,22 @@ simon.reset = function() {
 }
 
 function finalAnimation() {
-	$('body').css('overflow', 'hidden');
-	$('body').find('.board').addClass('rollEnd');
-	$('.center-wrapper').fadeOut(2000);
-	$('#displayTurn').children('h2').text('--');
+
+	$('.board, .center img').addClass('active');
+	$('.center-wrapper').fadeOut(500).parent('.center').find('img').addClass('active');
+	$('#displayTurn').children('h2').text('!!');
 	simon.reset;
 
 	setTimeout(function(){
-		$('.final').fadeIn(1500).delay(2000).fadeOut(500);
-	}, 2000);
+		$('.final').fadeIn(500).addClass('active').delay(10800).fadeOut(500);
+	}, 500);
 
 	$('.board').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-		$('body').css('overflow', 'auto');
-		$('.board').removeClass('rollEnd');
+		$('.board, .center img').removeClass('active');
 
 		setTimeout(function(){
-			$('.center-wrapper').fadeIn(400);
-		}, 400);
+			$('.center-wrapper').fadeIn(1000);
+		}, 100);
 	});
 }
 
@@ -130,7 +274,7 @@ let incrementalTimeout = function(i, _note, _playRate) {
 
 	}, i * _playRate);
 
-	timeouts.push(timeoutId); 	//console.log('timeouts arr: ' + timeouts);
+	timeouts.push(timeoutId);
 };
 
 
@@ -148,7 +292,7 @@ let playSeq = function() {
 	for (let i = 0; i < sequence.length; ++i) {
 		let note = sequence[i];
 		let playRate;
-		if (turn <= 15) playRate = 1250 - (50 * turn);
+		if (turn <= 15) playRate = 1035 - (35 * turn);
 		else playRate = 450;
 		incrementalTimeout(i, note, playRate);
 	}
@@ -161,7 +305,7 @@ let inputCheck = function(note) {
 		playSound(note);
 		copy.shift();
 
-		if (!copy.length && sequence.length >= 20) {
+		if (!copy.length && sequence.length >= 1) {
 			disableBoard();
 			setTimeout(function() {
 				finalAnimation()
@@ -191,6 +335,8 @@ let inputCheck = function(note) {
 	}
 }
 
+/* GAME BUTTONS, CONTROLS, EVENT LISTENERS */
+
 function enableControls() {
 	$('#displayTurn').children('h2').text('--').css('color', 'rgba(255, 56, 56, 1)');
 
@@ -219,16 +365,15 @@ function disableControls() {
 }
 
 $('.center').on('click', '.masterSwWrapper', function(){
-
 	if (!simon.masterSw) {
 		enableControls();
 		simon.masterSw = true;
 		simon.strict = false;
 		$('.masterSwWrapper').children('.masterSw').addClass('active');
-		$('.tile').fadeTo(0, 1, 'swing');
+		$('.tile').fadeTo(0, 1);
 		$('#startBtn').css('background-color', 'hsl(0, 100%, 63%)');
 		$('#strictBtn').css('background-color', 'hsl(48, 99%, 62%)');
-		$('.center').css('background-color', '#bbb');
+		$('.center-wrapper').css('background-color', 'rgba(196, 196, 196, .9)');
 	}
 	else {
 		simon.masterSw = false;
@@ -239,7 +384,7 @@ $('.center').on('click', '.masterSwWrapper', function(){
 		$('.tile').fadeTo(0, 0.7, 'swing');
 		$('#startBtn').css('background-color', 'hsl(0, 41%, 43%)');
 		$('#strictBtn').css('background-color', 'hsl(48, 46%, 41%)');
-		$('.center').css('background-color', '#999');
+		$('.center-wrapper').css('background-color', 'rgba(171, 171, 171, .8)');
 	}
 });
 
@@ -255,7 +400,6 @@ let boardActiveTime = function() {
 function enableBoard(){
 
 	boardActiveTime();
-
 	$('.board')
 		.on('click', '[data-tile]', function(e){
 			let btnSelectd = $(e.target).data('tile');
@@ -278,7 +422,11 @@ function disableBoard(){
 		.off('click touchend mousedown touchstart mouseup mouseout');
 }
 
-$('body').on('click', '#cog_icon', function(){
+$('.hi').on('click', function(){
+	$(this).addClass('flipOut');
+})
+
+$('body').on('click', '#infoIc, .overlay, .modal-wrapper', function(){
 	let overl = $('.overlay').hasClass('active');
 	if(!overl) {
 		$('body, .overlay, .modal-wrapper').removeClass('unactive');
